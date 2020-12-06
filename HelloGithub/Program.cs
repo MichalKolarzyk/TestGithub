83f3629ccb74f22fcde8b23da7632081760e4483
MichalKolarzyk <michalkolarzyk@gmail.com>
2020-12-06 17:46:09 +0100

    Add new feature

--- HelloGithub/Program.cs
+++ HelloGithub/Program.cs
@@ -19,6 +19,8 @@ namespace HelloGithub
         {
             Console.WriteLine("Hello Git Hub!");
             Console.WriteLine("This is from Git Hub");
+            Console.WriteLine("New feature");
+            Console.WriteLine("New feature efect");
         }
     }
 }
