ba878c39445c30915664b0eacd2aa6e5eaa13d9b
michal kolarzyk <michalkolarzyk@gmail.com>
2021-04-24 12:47:11 +0200

    Clear program file

--- HelloGithub/Program.cs
+++ HelloGithub/Program.cs
@@ -5,25 +5,9 @@ namespace HelloGithub
 {
     class Program
     {
-        public int Sub(int a, int b)
-        {
-            //bug fix
-            return a - b;
-        }
-        public int Add(int a, int b)
-        {
-            //Add Function
-            return a + b;
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git Hub!");
-            Console.WriteLine("This is from Git Hub");
-            Console.WriteLine("New feature");
-            Console.WriteLine("New feature efect");
-            Console.WriteLine("Merge from master");
-            Console.WriteLine("Merge from dev");
         }
     }
 }
