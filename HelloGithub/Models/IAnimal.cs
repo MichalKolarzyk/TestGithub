90ccc85a6d2510cc7a8dc976b2bf947979571778
michal kolarzyk <michalkolarzyk@gmail.com>
2021-04-24 12:50:50 +0200

    Add IAnimal class

---
+++ HelloGithub/Models/IAnimal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloGithub.Models
+{
+    public interface IAnimal
+    {
+        string Descripsion { get; set; }
+
+        string Sound { get; set; }
+    }
+}
