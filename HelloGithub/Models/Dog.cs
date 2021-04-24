aaba31a7cdcca610a459c16db5329e0cbf2e4d1a
michal kolarzyk <michalkolarzyk@gmail.com>
2021-04-24 12:59:52 +0200

    Add dog

---
+++ HelloGithub/Models/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloGithub.Models
+{
+    public class Dog : IAnimal
+    {
+        public string Descripsion => "I like my owner";
+
+        public string Sound => "wrrrrrr";
+    }
+}
